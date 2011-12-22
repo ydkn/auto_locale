a7f90b35a9faf6d6daa4293d86afb01db56f8803
Florian Schwab <me@ydkn.de>
2011-12-22 13:27:10 +0100

    Initial import

---
+++ auto_locale.gemspec
@@ -0,0 +1,18 @@
+# -*- encoding: utf-8 -*-
+$:.push File.expand_path("../lib", __FILE__)
+require "auto_locale/version"
+
+Gem::Specification.new do |s|
+  s.name        = "auto_locale"
+  s.version     = AutoLocale::VERSION
+  s.authors     = ["Florian Schwab"]
+  s.email       = ["me@ydkn.de"]
+  s.homepage    = "http://github.com/ydkn/auto_locale"
+  s.summary     = %q{Automatically set the locale from the browsers HTTP_ACCEPT_LANGUAGE header}
+  s.description = %q{Use this gem to automatically set the current locale from the browsers HTTP_ACCEPT_LANGUAGE header}
+
+  s.files         = `git ls-files`.split("\n")
+  s.test_files    = `git ls-files -- {test,spec,features}/*`.split("\n")
+  s.executables   = `git ls-files -- bin/*`.split("\n").map{ |f| File.basename(f) }
+  s.require_paths = ["lib"]
+end
---
+++ lib/auto_locale.rb
@@ -0,0 +1,55 @@
+# AutoLocale
+# Copyright (C) 2011 Florian Schwab
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+require "auto_locale/version"
+
+module AutoLocale
+  
+  def self.included base
+    base.before_filter :set_auto_locale
+  end
+  
+  def set_auto_locale
+    available_locales = I18n.available_locales.collect{|l| l.to_s}
+    
+    locales = request.accept_language.split(/\s*,\s*/).collect do |l|
+      l += ';q=1.0' unless l =~ /;q=\d+\.\d+$/
+      l.split(';q=')
+    end.sort do |a, b|
+      b.last.to_f <=> a.last.to_f
+    end.collect do |l|
+      l.first.downcase.gsub(/-[a-z]+$/i) {|c| c.upcase}
+    end
+    
+    locales.each do |l|
+      available_locales.each do |al|
+        if l == al
+          I18n.locale = al.to_sym
+          return
+        elsif l.split('-', 2).first == al.split('-', 2).first
+          I18n.locale = al.to_sym
+          return
+        end
+      end
+    end
+  rescue
+    I18n.locale = I18n.default_locale
+  end
+  
+end
+
+ActionController::Base.send :include, AutoLocale
