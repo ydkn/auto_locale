51c22270570f8bc23fe13c86430b9b4888f1cf02
Florian Schwab <me@ydkn.de>
2016-07-13 22:58:51 +0200

    * update readme * fix rspec

--- lib/auto_locale.rb
+++ lib/auto_locale.rb
@@ -1,5 +1,5 @@
 # AutoLocale
-# Copyright (C) 2011 Florian Schwab
+# Copyright (C) 2011-2016 Florian Schwab
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
